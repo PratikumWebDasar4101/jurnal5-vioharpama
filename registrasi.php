0dff8ec7c743713c198653d25fb699d60fa48f0d
vioharpama <42727462+vioharpama@users.noreply.github.com>
2018-10-01 10:06:49 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Form Registrasi</title>
+	<title></title>
 </head>
 <body>
-
-
-
+	<form>
+		<pre>
+			REGISTRASI
+			Nim	:<input type="text" name="nama" pattern="\d*" maxlength="10"><br><br>
+			Nama 	:<input type="text" name="nama" maxlength="25"><br><br>
+			Email 	:<input type="email" name="nama" maxlength="25"><br><br>
+			<input type="submit" value="KIRIM">
+		</pre>
+	</form>
 </body>
 </html>
+
+<?php
+	if(isset($_POST['nim'])){
+		$nim=$_POST['nim'];
+		$nama=$_POST['nama'];
+		$email=$_POST['email'];
+	}
+?>
+	<script>
+		alert("Registrasi Berhasil")
+	</script>
