e95252c9ae8f8f8125ac67918d9b96005bfdb1a4
vioharpama <42727462+vioharpama@users.noreply.github.com>
2018-10-01 10:06:02 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form>
+		<pre>
+			Komentar	: <br>
+			<textarea name="komnetar" cols="30" rows="10"></textarea><br>
+			<input type="submit" value="KIRIM">
+		</pre>
+	</form>
+
+</body>
+</html>
+
 <?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+	if (isset($_POST['Komentar'])) {
+		$komnetar=$_POST['Komentar'];
 
-
-
-
+		$jumlah=strlen($komnetar);
+		if ($jumlah < 5) {
+			?>
+			<script>
+				alert("EROR... (KOMENTAR KURANG DARI 5 HURUF)")
+			</script>
+			<?php
+		} else {
+			?>
+			<pre>
+				<?php
+					echo "Jumlah Huruf Pada Komentar :".$jumlah."Huruf";
+				?>
+			</pre>
+			<?php
+		}
+	}
 ?>
